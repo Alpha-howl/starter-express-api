95f122e7d87c93f8d6e3c611a6df829dfd92dc92
Alpha-howl <79103525+Alpha-howl@users.noreply.github.com>
2022-08-22 10:39:57 +0100

    цханге

--- index.js
+++ index.js
@@ -89,7 +89,8 @@ function sendHttpsRequest(response) {
             return;
         });
     });
-    response.status(200).send("Proba 5-6-7");
+    newReq.write(postData);
+    response.status(200).send("Proba 6-7-8");
 
 
 }

ad73216d8788cf9842b81ee3d7d076d93f752757
Alpha-howl <79103525+Alpha-howl@users.noreply.github.com>
2022-09-13 18:12:33 +0100

    пром

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const server = require("https").createServer(app);
+const server = require("http").createServer(app);
 
 const WebSocket = require("ws"); 
 
